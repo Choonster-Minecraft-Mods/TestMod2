9978875ff9ada12145fe18e9cc4472dc277d7bf0
Choonster <Choonster.2010@gmail.com>
2015-04-14 08:46:55 +1000

    Add Glowstone ToolMaterial
    -
    http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2401349-help-needed-setting-multiple-repair-items-in-one

--- src/main/java/com/choonster/testmod2/init/ItemRegistry.java
+++ src/main/java/com/choonster/testmod2/init/ItemRegistry.java
@@ -2,7 +2,10 @@ package com.choonster.testmod2.init;
 
 import com.choonster.testmod2.item.*;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemRegistry {
 	public static ItemColoredBed coloredBed;
@@ -15,6 +18,8 @@ public class ItemRegistry {
 	public static ItemExhaustionReset foodExhaustionResetter;
 	public static ItemStaff staff;
 	public static ItemChiselAndHammer chiselAndHammer;
+
+	public static final Item.ToolMaterial TOOL_MATERIAL_GLOWSTONE = EnumHelper.addToolMaterial("glowstone", 1, 5, 0.5f, 1.0f, 10).setRepairItem(new ItemStack(Items.glowstone_dust));
 
 	public static void registerItems() {
 		coloredBed = registerItem(new ItemColoredBed());
