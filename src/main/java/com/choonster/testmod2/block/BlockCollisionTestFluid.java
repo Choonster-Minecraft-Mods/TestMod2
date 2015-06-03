2c486be561fc125f2903d70c2620065679058197
Choonster <Choonster.2010@gmail.com>
2015-06-03 20:57:05 +1000

    Convert specific item when it collides with fluid
    - Converts Nether Quartz to Baked Potato, plays sound and spawns
    particles

--- src/main/java/com/choonster/testmod2/block/BlockCollisionTestFluid.java
+++ src/main/java/com/choonster/testmod2/block/BlockCollisionTestFluid.java
@@ -1,19 +1,17 @@
 package com.choonster.testmod2.block;
 
 import com.choonster.testmod2.TestMod2;
-import com.choonster.testmod2.util.ChatUtils;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 // A fluid Block that sends a server-wide chat message when an EntityItem collides with it
 // Test for this thread: http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2439366-help-with-have-items-be-swaped-with-other-items
@@ -36,8 +34,31 @@ public class BlockCollisionTestFluid extends BlockFluidClassic {
 
 	@Override
 	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
-		if (entity instanceof EntityItem && !world.isRemote) {
-			ChatUtils.sendServerMessage("Collision! %s", new SimpleDateFormat("HH:mm:ss").format(new Date()));
+		if (!(entity instanceof EntityItem)) { // If the Entity isn't an EntityItem, return now
+			return;
+		}
+
+		EntityItem entityItem = (EntityItem) entity; // Cast the Entity to EntityItem
+		ItemStack stack = entityItem.getEntityItem(); // Get the EntityItem's ItemStack
+
+		if (stack.getItem() == Items.quartz) { // If the stack contains Nether Quartz,
+			double posX = entityItem.posX, posY = entityItem.posY, posZ = entityItem.posZ;
+
+			if (!world.isRemote) { // If this is the server
+				ItemStack newStack = stack.copy(); // Copy the stack
+				newStack.func_150996_a(Items.baked_potato); // Replace its Item with Baked Potato
+
+				EntityItem newEntityItem = new EntityItem(world, posX, posY, posZ, newStack); // Create a new EntityItem at the same position
+				world.spawnEntityInWorld(newEntityItem); // Spawn it
+
+				entityItem.setDead(); // Kill the old one
+			}
+
+			world.playSoundAtEntity(entityItem, "game.tnt.primed", 1.0F, 1.0F); // Play a sound
+
+			for (int i = 0; i < 32; ++i) { // Spawn some particles
+				world.spawnParticle("portal", posX, posY + world.rand.nextDouble() * 2.0D, posZ, world.rand.nextGaussian(), 0.0D, world.rand.nextGaussian());
+			}
 		}
 	}
 }
