ff5044e8930fd5c392a38eee0c087a7fe05ebf87
Choonster <Choonster.2010@gmail.com>
2015-06-11 01:43:19 +1000

    Uncomment Super TNT Facade Blacklist

--- src/main/java/com/choonster/testmod2/compat/BCCompat.java
+++ src/main/java/com/choonster/testmod2/compat/BCCompat.java
@@ -4,8 +4,9 @@ import com.choonster.testmod2.init.BlockRegistry;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import net.minecraft.item.ItemStack;
 
+// http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2446431-blacklisting-buildcraft-facades-in-the-latest
 public class BCCompat {
 	public static void init(){
-		//FMLInterModComms.sendMessage("BuildCraft|Transport", "blacklist-facade", new ItemStack(BlockRegistry.superTNT));
+		FMLInterModComms.sendMessage("BuildCraft|Transport", "blacklist-facade", new ItemStack(BlockRegistry.superTNT));
 	}
 }
