5196607aa17bb1892425e49302b1ad86bd312b6f
Choonster <Choonster.2010@gmail.com>
2015-06-27 03:49:22 +1000

    Reduce update frequency of Entities

--- src/main/java/com/choonster/testmod2/init/Entities.java
+++ src/main/java/com/choonster/testmod2/init/Entities.java
@@ -16,14 +16,14 @@ public class Entities {
 	private static void registerEntity(Class<? extends Entity> entityClass, String entityName, int bgRed, int bgGreen, int bgBlue, int fgRed, int fgGreen, int fgBlue) {
 		int entityID = EntityRegistry.findGlobalUniqueEntityId();
 		EntityRegistry.registerGlobalEntityID(entityClass, entityName, entityID, getRGBInt(bgRed, bgGreen, bgBlue), getRGBInt(fgRed, fgGreen, fgBlue));
-		EntityRegistry.registerModEntity(entityClass, entityName, entityID, TestMod2.instance, 64, 3, true);
+		EntityRegistry.registerModEntity(entityClass, entityName, entityID, TestMod2.instance, 64, 1, true);
 	}
 
 	// Register an Entity without a spawn egg
 	private static void registerEntity(Class<? extends Entity> entityClass, String entityName) {
 		int entityID = EntityRegistry.findGlobalUniqueEntityId();
 		EntityRegistry.registerGlobalEntityID(entityClass, entityName, entityID);
-		EntityRegistry.registerModEntity(entityClass, entityName, entityID, TestMod2.instance, 64, 3, true);
+		EntityRegistry.registerModEntity(entityClass, entityName, entityID, TestMod2.instance, 64, 1, true);
 	}
 
 	private static int getRGBInt(int rInt, int gInt, int bInt) {
