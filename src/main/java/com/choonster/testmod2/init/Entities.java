f0948e6d4aa03da4b10052e8d3adb23daffd9c79
Choonster <Choonster.2010@gmail.com>
2015-07-13 05:14:19 +1000

    Add Armoured Skeleton spawn to Test biome

--- src/main/java/com/choonster/testmod2/init/Entities.java
+++ src/main/java/com/choonster/testmod2/init/Entities.java
@@ -7,6 +7,7 @@ import com.choonster.testmod2.entity.EntityBarrelBomb;
 import com.choonster.testmod2.entity.EntityZombieTest;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EnumCreatureType;
 
 public class Entities {
 	private static int entityID = 0;
@@ -15,6 +16,8 @@ public class Entities {
 		registerEntity(EntityZombieTest.class, "TestZombie", 96, 23, 173, 14, 2, 235);
 		registerEntity(EntityBarrelBomb.class, "BarrelBomb");
 		registerEntity(EntityArmouredSkeleton.class, "ArmouredSkeleton", 66, 66, 66, 88, 88, 88);
+
+		EntityRegistry.addSpawn(EntityArmouredSkeleton.class, 1000, 1, 10, EnumCreatureType.monster, ModBiomes.biomeGenTest);
 	}
 
 	// Register an Entity with a spawn egg
