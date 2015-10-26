4137ed1ccdbb5c0c0eb17e2fa3fa82b9363f5d7c
Choonster <Choonster.2010@gmail.com>
2015-10-27 03:37:00 +1100

    Add OreDictStuff.getOreNameForOreList
    - Add doc comment for OreDictStuff.getOreNames

--- src/main/java/com/choonster/testmod2/crafting/OreDictStuff.java
+++ src/main/java/com/choonster/testmod2/crafting/OreDictStuff.java
@@ -2,8 +2,18 @@ package com.choonster.testmod2.crafting;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.List;
 
 public class OreDictStuff {
+
+	/**
+	 * Gets an array of ore names for the specified ore.
+	 *
+	 * @param itemStack The ore
+	 * @return An array of ore names
+	 */
 	public static String[] getOreNames(ItemStack itemStack) {
 		int[] ids = OreDictionary.getOreIDs(itemStack);
 		String[] names = new String[ids.length];
@@ -12,7 +22,36 @@ public class OreDictStuff {
 			names[i] = OreDictionary.getOreName(ids[i]);
 		}
 
-
 		return names;
+	}
+
+	/**
+	 * Gets the ore name corresponding to the specified list of ores.
+	 *
+	 * @param ores The list of ores
+	 * @return The corresponding ore name
+	 * @throws InvalidOreException When no matching ore name is found
+	 */
+	public static String getOreNameForOreList(List<ItemStack> ores) throws InvalidOreException {
+		for (String possibleOreName : getOreNames(ores.get(0))) {
+			if (ores.equals(OreDictionary.getOres(possibleOreName))) {
+				return possibleOreName;
+			}
+		}
+
+		throw new InvalidOreException("Unable to find name for ores " + ArrayUtils.toString(ores.toArray()));
+	}
+
+	public static class InvalidOreException extends Exception {
+		public InvalidOreException() {
+		}
+
+		public InvalidOreException(String message) {
+			super(message);
+		}
+
+		public InvalidOreException(String message, Throwable cause) {
+			super(message, cause);
+		}
 	}
 }
