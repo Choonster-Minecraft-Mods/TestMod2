16a41f0660a602ab915fa1f5e0dcad3a452023c5
Choonster TheMage <Choonster.2010@gmail.com>
2016-04-09 14:29:14 +1000

    Block Debugger: Use raytracing from onItemRightClick

--- src/main/java/com/choonster/testmod2/item/ItemBlockDebugger.java
+++ src/main/java/com/choonster/testmod2/item/ItemBlockDebugger.java
@@ -8,6 +8,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 /**
@@ -20,19 +21,41 @@ public class ItemBlockDebugger extends Item {
 		setTextureName("minecraft:nether_star");
 	}
 
-	@Override
-	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
-		Block block = world.getBlock(x, y, z);
-		int meta = world.getBlockMetadata(x, y, z);
-		TileEntity tileEntity = world.getTileEntity(x, y, z);
+	/**
+	 * Print the block at the specified coordinates to the log.
+	 *
+	 * @param world The World
+	 * @param x     The x coordinate
+	 * @param y     The y coordinate
+	 * @param z     The z coordinate
+	 */
+	private void logBlock(World world, int x, int y, int z) {
+		final Block block = world.getBlock(x, y, z);
+		final int meta = world.getBlockMetadata(x, y, z);
+		final TileEntity tileEntity = world.getTileEntity(x, y, z);
 
 		Logger.info("Block at %d,%d,%d: %s (meta %d) - Registry name: %s", x, y, z, block, meta, Block.blockRegistry.getNameForObject(block));
 		if (tileEntity != null) {
-			NBTTagCompound tagCompound = new NBTTagCompound();
+			final NBTTagCompound tagCompound = new NBTTagCompound();
 			tileEntity.writeToNBT(tagCompound);
 			Logger.info("TileEntity data: %s", tagCompound);
 		}
+	}
 
-		return true;
+	@Override
+	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+//		logBlock(world, x, y, z);
+
+		return false;
+	}
+
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer player) {
+		MovingObjectPosition result = getMovingObjectPositionFromPlayer(worldIn, player, true);
+		if (result != null && result.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK){
+			logBlock(worldIn, result.blockX, result.blockY, result.blockZ);
+		}
+
+		return itemStackIn;
 	}
 }
